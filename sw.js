ebd516fb5fdb1a8050d53a2cd7810045cf45a5f9
sks <blueroof@jei.com>
2025-01-08 15:00:40 +0900

    푸쉬메세지 수정

--- sw.js
+++ sw.js
@@ -2,6 +2,7 @@ const CACHE_NAME = "sauna-counter-v1";
 const assets = ["/", "/index.html", "/icon.svg"];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
   );
@@ -12,5 +13,20 @@ self.addEventListener("fetch", (event) => {
     caches
       .match(event.request)
       .then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: "window" }).then((windowClients) => {
+      if (windowClients.length > 0) {
+        windowClients[0].focus();
+      }
+    })
   );
 });
