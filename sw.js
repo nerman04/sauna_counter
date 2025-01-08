622afabc0f583d425b5b4c55d81fde3a8c31e00f
sks <blueroof@jei.com>
2025-01-08 15:09:49 +0900

    푸쉬메세지 구문 제거

--- sw.js
+++ sw.js
@@ -19,14 +19,3 @@ self.addEventListener("fetch", (event) => {
 self.addEventListener("activate", (event) => {
   event.waitUntil(clients.claim());
 });
-
-self.addEventListener("notificationclick", (event) => {
-  event.notification.close();
-  event.waitUntil(
-    clients.matchAll({ type: "window" }).then((windowClients) => {
-      if (windowClients.length > 0) {
-        windowClients[0].focus();
-      }
-    })
-  );
-});
